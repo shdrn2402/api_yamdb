c09c596429813e2d912b7eead7dc68279b465dae
Alexandr <belinskii1@yandex.ru>
2022-01-10 16:27:38 +0300

    permitions

--- api_yamdb/api/permisions.py
+++ api_yamdb/api/permisions.py
@@ -2,10 +2,16 @@ from rest_framework import permissions
 
 
 class IsAuthor(permissions.BasePermission):
-    pass
+
+    def has_object_permission(self, request, view, obj):
+        return (
+            obj.author == request.user
+            or request.method in permissions.SAFE_METHODS
+        )
 
 
 class IsModerator(permissions.BasePermission):
+    
     pass
 
 
