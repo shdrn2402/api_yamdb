e8bd449f6a4eae07c13cf15a2abe8a04ad58b9a9
Екатерина <catya.drozdova@yandex.ru>
2022-01-12 23:49:09 +0300

    add ReviewSerializer validation

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import serializers
 
 from reviews.models import Category, Comment, Genre, Review, Title, User
@@ -16,6 +17,17 @@ class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
         fields = ('id', 'text', 'author', 'score', 'pub_date')
+
+    def validate(self, data):
+        title = get_object_or_404(Title, pk=self.kwargs.get('review_id'))
+        if self.request.method == 'POST':
+            if Review.objects.filter(
+                    title=title,
+                    author=self.request.user
+            ).exists():
+                raise serializers.ValidationError(
+                    'Можно оставить только один отзыв на произведение')
+        return data
 
 
 class CommentSerializer(serializers.ModelSerializer):
