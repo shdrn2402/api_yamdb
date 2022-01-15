84d7156b69889d28e60c0ff3360c49f5e6769ebc
Alexandr <belinskii1@yandex.ru>
2022-01-16 00:05:32 +0300

    поправил валидатор User

--- api_yamdb/reviews/utils.py
+++ api_yamdb/reviews/utils.py
@@ -3,4 +3,4 @@ from django.core.exceptions import ValidationError
 
 def username_validation(username):
     if username == 'me':
-        raise ValidationError("Username can't be 'me'")
+        raise ValidationError("Нельзя использовать имя 'me'")
